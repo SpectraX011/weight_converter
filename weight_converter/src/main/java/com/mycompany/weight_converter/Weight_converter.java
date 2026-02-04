0ba036974ed6df0c49a7b7b64668ed06d571993f
Daniel S. Prudenciado <218104746+SpectraX011@users.noreply.github.com>
2026-02-04 21:03:55 +0800

    Add files via upload

---
+++ weight_converter/src/main/java/com/mycompany/weight_converter/Weight_converter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.weight_converter;
+
+/**
+ *
+ * @author Nullroot 13
+ */
+public class Weight_converter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
